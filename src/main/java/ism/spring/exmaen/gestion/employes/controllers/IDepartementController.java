ff835d81f6d46dabf7da33074463de327eb92d50
kephrr <kephnze@gmail.com>
2025-04-11 20:46:35 +0000

    add: form departemtns, CU departements

--- src/main/java/ism/spring/exmaen/gestion/employes/controllers/IDepartementController.java
+++ src/main/java/ism/spring/exmaen/gestion/employes/controllers/IDepartementController.java
@@ -1,8 +1,8 @@
 package ism.spring.exmaen.gestion.employes.controllers;
 
-import ism.spring.exmaen.gestion.employes.controllers.form.EmployesForm;
+import ism.spring.exmaen.gestion.employes.controllers.form.DepartementsFrom;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/departements")
-public interface IDepartementController extends IController<String, EmployesForm>{
+public interface IDepartementController extends IController<String, DepartementsFrom>{
 }
